bdd08196dc0fa561f483d8ea80b98ab715783333
Aero <you@example.com>
2024-12-09 10:26:50 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="text-veryDarkCyan text-8xl font-bold">Hello World</h1>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      strongCyan: "#26c0ab",
+      veryDarkCyan: "#00494d",
+      darkGrayishCyan: "#5e7a7d",
+      grayishCyan: "#7f9c9f",
+      veryLightGrayishCyan: "#f4fafa",
+      white: "#ffffff",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
